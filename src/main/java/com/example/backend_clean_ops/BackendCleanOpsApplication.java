8d04e25d7f09919bc5f86797cb3813001b1297ea
Ondrej0 <ondrejmuzik27@gmail.com>
2026-06-23 19:54:05 +0100

    Initial Commit

---
+++ src/main/java/com/example/backend_clean_ops/BackendCleanOpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_clean_ops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCleanOpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCleanOpsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backend_clean_ops/BackendCleanOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_clean_ops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCleanOpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
