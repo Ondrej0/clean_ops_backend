f0dec31806fee55bd726d8f6bbf0e9d3a11ea27f
Ondrej0 <ondrejmuzik27@gmail.com>
2026-07-07 21:28:09 +0100

    Big clean up, simplifying layout

--- src/main/java/com/example/backend_clean_ops/controller/CleanerController.java
+++ src/main/java/com/example/backend_clean_ops/controller/CleanerController.java
@@ -1,14 +1,14 @@
 package com.example.backend_clean_ops.controller;
 
-import com.example.backend_clean_ops.service.ShiftService;
+import com.example.backend_clean_ops.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/shifts")
+@RequestMapping("/api/schedules")
 @RequiredArgsConstructor
-public class ShiftController {
+public class CleanerController {
 
-    private final ShiftService shiftService;
+    private final ScheduleService scheduleService;
 }
--- src/main/java/com/example/backend_clean_ops/service/ShiftService.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend_clean_ops.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ShiftService {
-}
