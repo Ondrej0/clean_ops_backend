fba60720d1ed8a48b524a93db6e1b60598ffe452
Ondrej0 <ondrejmuzik27@gmail.com>
2026-09-03 14:52:32 +0100

    add little TODO and a test to api scratch

--- src/main/java/com/example/backend_clean_ops/dto/responses/GetSitesResponse.java
+++ src/main/java/com/example/backend_clean_ops/dto/responses/GetSitesResponse.java
@@ -1,7 +1,7 @@
 package com.example.backend_clean_ops.dto.responses;
 
 import java.util.List;
-
+//TODO change sites to be small S, also have to update fronet end so tehre is a match
 public record GetSitesResponse(
         List<SiteResponse> Sites
 ) {
